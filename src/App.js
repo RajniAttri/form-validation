a1543c77eb787d5f005e8d066d96a42ae5eafebd
RajniAttri <rajniattri72003@gmail.com>
2024-06-06 21:08:07 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, HashRouter } from 'react-router-dom';
 import Form from './components/Form';
 import Success from './components/Success';
 
 const App = () => {
   return (
+    <>
+
+    <HashRouter>
     <Router>
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/success" element={<Success />} />
       </Routes>
     </Router>
+    </HashRouter>
+
+    </>
   );
 };
 
